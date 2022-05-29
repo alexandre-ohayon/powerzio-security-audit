dfff74d03d9712dc3ca08f2cc9dfca371c4a0754
Alexandre O <alexandre.ohayon@epitech.eu>
2022-05-30 00:46:14 +0200

    Update dump_redis.py

--- sensitive_files/database.powerzio.lan/dump_redis.py
+++ sensitive_files/database.powerzio.lan/dump_redis.py
@@ -9,8 +9,12 @@ def show_passwd(user_id):
     i = 0
     end = len(user_id)
     string = []
+    special_char = ')'
     while i < end:
-       string.append(chr(ord(user_id[i]) - 9)) # ord() function converts a character into an integer that represents the Unicode code of the character, the chr() function converts a Unicode code character into the corresponding string, -9 is because we saw there is + -9 on the password generation of pmanager.
+       if user_id[i] == special_char:
+            string.append("A")  # 0x41 we found in the pmanager decompiled
+        else:
+            string.append(chr(ord(user_id[i]) - 9)) # ord() function converts a character into an integer that represents the Unicode code of the character, the chr() function converts a Unicode code character into the corresponding string, -9 is because we saw there is + -9 on the password generation of pmanager.
        i = i + 1
     return "".join(string)
 
